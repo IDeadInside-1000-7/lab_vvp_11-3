3272b63a851ff0b20187b7ef31a41a48311e22cf
IDeadInside-1000-7 <90912641+IDeadInside-1000-7@users.noreply.github.com>
2021-11-09 16:47:19 +0300

    Update Source.cpp

--- lab_vvp_11-3/Source.cpp
+++ lab_vvp_11-3/Source.cpp
@@ -2,24 +2,32 @@
 #include <math.h>
 int main()
 {
-	int A, B, C, summ;
-	printf("A = ");
-	scanf("%i", &A);
-	printf("B = ");
-	scanf("%i", &B);
-	printf("C = ");
-	scanf("%i", &C);
-	int t1 = abs(A - B);
-	int t2 = abs(A - C);
-	if (t1 > t2)
+	int Ax, Ay, Bx, By, Cx, Cy;
+	printf("Enter coordinates of A:");
+	printf("\nAx = ");
+	scanf("%i", &Ax);
+	printf("\nAy = ");
+	scanf("%i", &Ay);
+	printf("\nEnter coordinates of B:");
+	printf("\nBx = ");
+	scanf("%i", &Bx);
+	printf("\nBy = ");
+	scanf("%i", &By);
+	printf("\nEnter coordinates of C:");
+	printf("\nCx = ");
+	scanf("%i", &Cx);
+	printf("\nCy = ");
+	scanf("%i", &Cy);
+	int AB = sqrt((pow((Bx - Ax), 2)) + (pow((By - Ay), 2)));
+	int AC = sqrt((pow((Cx - Ax), 2)) + (pow((Cy - Ay), 2)));
+	if (AB < AC)
 	{
-		printf("abs(A - C) = %i (Distance)", t2);
-		printf("\nC = %i", C);
+		printf("\nThat point is C:\n");
+		printf("AC = %i (Distance between A and C)", AC);
 	}
-	else if (t1 < t2)
+	else if (AB > AC)
 	{
-		printf("abs(A - B) = %i (Distance)", t1);
-		printf("\nB = %i", B);
+		printf("\nThat point is B:\n");
+		printf("AB = %i (Distance between A and B)", AB);
 	}
-	
 }
